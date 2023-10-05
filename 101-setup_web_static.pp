1a5dce543825c48aad776641ce61c92e6b31d4d7
A-fethi <fethiabderrahmane1@gmail.com>
2023-10-05 04:06:04 -0700

    Puppet for setup

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,45 @@
+# Redoing the task #0 but by using Puppet
+
+$nginx_conf = "server {
+	listen 80 default_server;
+	location /hbnb_static/ {
+		alias /data/web_static/current/;
+		index index.html;
+	}
+}"
+
+package { 'nginx':
+  ensure   => 'present',
+  provider => 'apt',
+}
+
+file { '/data/web_static/shared/':
+  ensure => directory,
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+}
+
+file { '/data/web_static/releases/test/':
+  ensure => directory,
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+}
+
+file { '/data/web_static/releases/test/index.html':
+  ensure  => present,
+  content => 'Hello World!!',
+}
+
+file { '/data/web_static/current':
+  ensure => link,
+  target => '/data/web_static/releases/test/',
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => present,
+  content => $nginx_conf,
+}
+
+exec { 'nginx restart':
+  path => '/etc/init.d/'
+}
