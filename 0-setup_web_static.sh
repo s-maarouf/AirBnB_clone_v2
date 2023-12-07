8cea9838a261d1edf2ddac32a64108d466d42ac3
s-maarouf <maarouf.salahedd@gmail.com>
2023-12-07 13:50:13 +0100

    Fixed html content

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,7 +4,15 @@ sudo apt update -y
 sudo apt install nginx -y
 mkdir -p '/data/web_static/releases/test/'
 mkdir -p '/data/web_static/shared/'
-echo "Hello World!!" > '/data/web_static/releases/test/index.html'
+echo "
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>
+" > '/data/web_static/releases/test/index.html'
 ln -sf '/data/web_static/releases/test/' '/data/web_static/current'
 sudo chown -R ubuntu:ubuntu /data/
 printf %s "server {
